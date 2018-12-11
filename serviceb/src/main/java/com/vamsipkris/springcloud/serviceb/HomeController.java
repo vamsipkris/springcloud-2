05af71ad38af342fcc3dafed830baf299ef171d9
vamsi krishna <vkrishna.p@gmail.com>
2018-12-12 01:21:18 +0530

    Load Balanced Inter Microservice calls

--- serviceb/src/main/java/com/vamsipkris/springcloud/serviceb/HomeController.java
+++ serviceb/src/main/java/com/vamsipkris/springcloud/serviceb/HomeController.java
@@ -1,7 +1,9 @@
 package com.vamsipkris.springcloud.serviceb;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by vamsikrishna on 11/12/18.
@@ -9,8 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/rest")
 public class HomeController {
+    @Autowired
+    private RestTemplate restTemplate;
+
     @RequestMapping("/hello")
     public String home() {
-        return "Hello I am Service B";
+        String ret="Service B: Message from Service A: ";
+        String msgFromA = restTemplate.getForObject("http://servicea/rest/hello",String.class);
+        return ret + msgFromA;
     }
 }
--- serviceb/src/main/java/com/vamsipkris/springcloud/serviceb/ServiceBApplication.java
+++ serviceb/src/main/java/com/vamsipkris/springcloud/serviceb/ServiceBApplication.java
@@ -3,7 +3,10 @@ package com.vamsipkris.springcloud.serviceb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by vamsikrishna on 11/12/18.
@@ -15,4 +18,10 @@ public class ServiceBApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceBApplication.class, args);
     }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+        }
 }
